cee5dbf320da186d7985ba31611494a2635150a4
Madhusree <madhusree.dits@gmail.com>
2025-11-13 14:22:39 +0530

    Updated 1313

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
 
   return {
-    base: '/pracd/', 
+    base: './', 
     plugins: [react()],
     server: {
       proxy: {
