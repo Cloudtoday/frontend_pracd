56a2ae819ce6f9b3c5ae98ac9d703ff95e9eaac0
Madhusree <madhusree.dits@gmail.com>
2025-11-13 14:16:41 +0530

    Updated 1312

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,17 @@
+import process from 'node:process';
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ mode }) => {
-  // ✅ Load env variables based on mode (development / production)
   const env = loadEnv(mode, process.cwd(), '')
 
   return {
-    base: './', // ✅ works on Netlify
+    base: '/pracd/', 
     plugins: [react()],
     server: {
       proxy: {
         '/auth': {
-          // ✅ Now use env safely
-          target: env.VITE_API_URL || 'http://localhost:5000',
+          target: env.VITE_API_URL,
           changeOrigin: true,
         },
       },
